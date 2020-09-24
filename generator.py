fa75a2ae61345f41404787896731cb3d346c686b
Joseph <josdavil@espol.edu.ec>
2020-09-24 15:06:21 -0500

    builder header, add_images

--- generator.py
+++ generator.py
@@ -1,10 +1,30 @@
 
 
 
+class Genarator:
+    def __init__(self, user,repositorie):
+        self.user=user
+        self.repositorie=repositorie
 
-def build_header(user,repositorie,name="logo.png"):
-    return "![header](https://raw.githubusercontent.com/"+user+"/"+repositorie+"/master/images/"+name+")"
-def create_readme(add):
-    file_readme=open("README.md","a")
-    file_readme.write(add)
-    file_readme.close
+    def build_header(user,repositorie,name="logo.png"):
+        return "![header](https://raw.githubusercontent.com/"+user+"/"+repositorie+"/master/images/"+name+")"
+    
+    def create_readme():
+        file_readme=open("README.md","a")
+        file_readme.close()
+    
+    def title(self,title):
+        file_readme=open("README.md","a")
+        file_readme.write("# "+title)
+        file_readme.close()
+
+    def description(self,description):
+        file_readme=open("README.md","a")
+        file_readme.write(description)
+        file_readme.close()
+
+        
+    def add_image(user,repositorie,name="logo.png"):
+        file_readme=open("README.md","a")
+        file_readme.write("![header](https://raw.githubusercontent.com/"+user+"/"+repositorie+"/master/images/"+name+")")
+        file_readme.close()
